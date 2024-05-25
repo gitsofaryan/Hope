9de1e606f85926a10a79ed1f1e1b67f388fcf2a2
Abhishek singh chauhan <avi7000201383@gmail.com>
2024-05-26 00:15:25 +0530

    iniial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+import Page from "./Page";
+
+const App = () => {
+  return (
+    <>
+      <Page />
+    </>
+  );
+};
+
+export default App;
---
+++ src/Page.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Sidebar from "./components/Sidebar/Sidebar";
+import Main from "./components/Main/Main";
+
+const Page = () => {
+  return (
+    <>
+      <Sidebar />
+      <Main />
+    </>
+  );
+};
+
+export default Page;
